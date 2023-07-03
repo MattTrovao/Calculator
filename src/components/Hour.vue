47ac35690422aecddfd8ec8f9dad38fd0861aed7
MattTrovao <42043818+MattTrovao@users.noreply.github.com>
2023-07-03 15:05:35 -0300

    fix calculo de Horas (seleciona o total de dias trabalhados)

--- src/components/Hour.vue
+++ src/components/Hour.vue
@@ -8,11 +8,22 @@
       <label for="hours">Total de Horas (por semana)</label>
 
       <select id="hours" v-model="workHours">
-        <option value="44">44 Horas (8h)</option>
+        <option value="48">48 Horas (8h)</option>
         <option value="36">36 Horas (6h)</option>
         <option value="20">20 Horas (4h)</option>
       </select>
     </fieldset>
+
+    <fieldset class="fields__group">
+      <label for="days">Dias trabalhados (por semana)</label>
+
+      <select id="days" v-model="workDays">
+        <option value="5">5 dias</option>
+        <option value="6">6 dias</option>
+        <option value="7">7 dias</option>
+      </select>
+    </fieldset>
+
 
     <fieldset class="fields__group">
       <label for="wage">Salário</label>
@@ -46,7 +57,7 @@
   <div class="calc">
     <p>
       Formula utilizada: <br>
-      <b>Salário / [ (Horas Trabalhadas / 6) * 30 ]</b>
+      <b>Salário / [ (Horas Trabalhadas / Dias Trabalhados) * 30 ]</b>
     </p>
   </div>
 </template>
@@ -54,12 +65,13 @@
 <script setup>
 import { ref } from "vue";
 
-const workHours = ref(44);
+const workHours = ref(48);
+const workDays = ref(5)
 const totalWage = ref();
 
 const hourValue = ref(null)
 
 const getHourValue = () => {
-  hourValue.value = totalWage.value / ( (workHours.value / 6) * 30 )
+  hourValue.value = totalWage.value / ( (workHours.value / workDays.value) * 30 )
 }
 </script>
